463ec73c25cce850e98639c439084984f23a90ca
podryna <podryna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-04 18:04:10 +0000

    From ticket 2280, new plugin version.
    Assigned to Rostislav. He said that previous developer used some functions that appeared in new version, also find bugs. He fixed that plugin.
    
    Added plugin to SocialEars Corp. site and to http://marketing-tools-tips-resources.com/ 
    
    It works. We checked it on WP3.5. It works too.
    
    
    git-svn-id: https://plugins.svn.wordpress.org/social-analytics-and-content-seo-using-socialears/trunk@869389 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- se_content.php
+++ se_content.php
@@ -8,180 +8,157 @@ Author: SocialEars
 Author URI: http://www.socialears.com
 */
 
-wp_enqueue_script('content-analyze','/wp-content/plugins/social_ears/js/functions.js');
+wp_enqueue_script('content-analyze', '/wp-content/plugins/social_ears/js/functions.js');
 
 /**
- * Install default settings 
+ * Install default settings
  */
-function social_ears_activate() {
-	$defaults_array = array(
-		"analyze_url" => "https://seg1.socialears.com/SE/SE_web/web/content_analyzer",
-		"title_generator_url" => "https://seg1.socialears.com/SE/SE_web/web/BlogTitleGenerator",
-	);
-	add_option( 'se_option', $defaults_array );
+function social_ears_activate()
+{
+    $defaults_array = array(
+        "analyze_url" => "https://seg1.socialears.com/SE/SE_web/web/content_analyzer",
+        "title_generator_url" => "https://seg1.socialears.com/SE/SE_web/web/BlogTitleGenerator",
+    );
+    add_option('se_option', $defaults_array);
 }
-register_activation_hook( __FILE__, 'social_ears_activate' );
+
+register_activation_hook(__FILE__, 'social_ears_activate');
 
 /**
  *  Deactivate plugin
  */
-function social_ears_deactivate() {
-	delete_option('se_option');
+function social_ears_deactivate()
+{
+    delete_option('se_option');
 }
-register_deactivation_hook( __FILE__, 'social_ears_deactivate' );
+
+register_deactivation_hook(__FILE__, 'social_ears_deactivate');
 
 /**
- * Add analyze sidebar to the edit page 
+ * Add analyze sidebar to the edit page
  */
-function social_ears_sidebar() {
-	add_meta_box( 'social_ears', 'Content Analyze', 'social_ears_sidebar_html', 'post', 'side' );
+function social_ears_sidebar()
+{
+    add_meta_box('social_ears', 'Content Analyze', 'social_ears_sidebar_html', 'post', 'side');
 }
-add_action( 'add_meta_boxes', 'social_ears_sidebar' );
+
+add_action('add_meta_boxes', 'social_ears_sidebar');
 
 /**
  * Analyze sidebar HTML code
- * @param type $post 
+ * @param type $post
  */
-function social_ears_sidebar_html( $post ){
-	$se_options = get_option( 'se_option' );
+function social_ears_sidebar_html($post)
+{
+    $se_options = get_option('se_option');
 
-	echo '<input type="hidden" id="analyze_url" value="'. $se_options["analyze_url"] .'" />
-		<input type="hidden" id="title_generator_url" value="'. $se_options["title_generator_url"] .'" />
+    echo '<input type="hidden" id="analyze_url" value="' . $se_options["analyze_url"] . '" />
+		<input type="hidden" id="title_generator_url" value="' . $se_options["title_generator_url"] . '" />
 		<style>#social_ears{display:none}</style>';
 }
 
 /**
  * Settings button on plugins panel
  */
-function social_ears_links($links, $file) {
-	static $this_plugin;
-	if( !$this_plugin ){ $this_plugin = plugin_basename( __FILE__ ); }
+function social_ears_links($links, $file)
+{
+    static $this_plugin;
+    if (!$this_plugin) {
+        $this_plugin = plugin_basename(__FILE__);
+    }
 
-	if( $file == $this_plugin ) {
-		$settings_link = '<a href="options-general.php?page=social-ears">Settings</a>';
-		array_unshift( $links, $settings_link );
-	}	
-	return $links;
+    if ($file == $this_plugin) {
+        $settings_link = '<a href="options-general.php?page=social-ears">Settings</a>';
+        array_unshift($links, $settings_link);
+    }
+    return $links;
 }
-add_filter( 'plugin_action_links', 'social_ears_links', 10, 2 );
+
+add_filter('plugin_action_links', 'social_ears_links', 10, 2);
 
 
 /* Main class */
+
 class social_ears
 {
-	/**
-	* Holds the values to be used in the fields callbacks
-	*/
-	private $options;	
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
 
-	/**
-	*  Init
-	*/
-	public function __construct() {		
-		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
-		add_action( 'admin_init', array( $this, 'page_init' ) );
-	}
-
-	/**
-	* Add options page
-	*/
-	public function add_plugin_page() {
-		// This page will be under "Settings"
-		add_options_page(
-			'Social Ears Settings', 
-			'Social Ears Analyze', 
-			'manage_options', 
-			'social-ears', 
-			array( $this, 'create_admin_page' )
-		);
-	}
-
-	/**
-	* Options page callback
-	*/
-	public function create_admin_page() {
-		// Set class property
-		$this->options = get_option( 'se_option' );
-	?>
-		<div class="wrap">
-		<?php screen_icon(); ?>
-		<h2>Social Ears Settings</h2>   
-		<form method="post" action="options.php">
-		<?php
-			// This prints out all hidden setting fields
-			settings_fields( 'se_option_group' );   
-			do_settings_sections( 'social-ears' );
-			submit_button(); 
-		?>
-		</form>
-		</div>
-	<?php
-	}
-
-	/**
-	* Register and add settings
-	*/
-	public function page_init(){
-		register_setting(
-		'se_option_group', // Option group
-		'se_option', // Option name
-		array( $this, 'sanitize' ) // Sanitize
-	);
-
-	add_settings_section(
-		'se_setting_section_id', // ID
-		'', // Title
-		'settings_content', // Callback
-		'social-ears' // Page
-	);
-	add_settings_field(
-		'analyze_url', 
-		'Analyze URL', 
-		array( $this, 'analyze_url_callback' ), 
-			'social-ears', 
-			'se_setting_section_id'
-		);
-
-    add_settings_field(
-        'title_generator_url',
-        'Title generator URL',
-        array( $this, 'title_generator_url_callback' ),
-        'social-ears',
-        'se_setting_section_id'
-    );
-
-    //used in add_settings_section
-    function settings_content(){
-
-    }
+    /**
+     *  Init
+     */
+    public function __construct()
+    {
+        add_action('admin_menu', array($this, 'add_plugin_page'));
     }
 
-	/**
-	* Sanitize each setting field as needed
-	* @param array $input Contains all settings fields as array keys
-	*/
-	public function sanitize( $input ) {
-		if( !empty( $input['analyze_url'] ) ){
-			$input['analyze_url'] = esc_url( $input['analyze_url'] );
-		}
-        if( !empty( $input['title_generator_url'] ) ){
-            $input['title_generator_url'] = esc_url( $input['title_generator_url'] );
+    /**
+     * Add options page
+     */
+    public function add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Social Ears Settings',
+            'Social Ears Analyze',
+            'manage_options',
+            'social-ears',
+            array($this, 'create_admin_page')
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page()
+    {
+
+        if (isset($_POST['se_submit'])) {
+            $updated_array = array(
+                "analyze_url" => sanitize_text_field($_POST['se_option']['analyze_url']),
+                "title_generator_url" => sanitize_text_field($_POST['se_option']['title_generator_url']),
+            );
+
+            update_option('se_option', $updated_array); ?>
+            <div class="updated"><p><strong>Social Ears Settings were updated</strong></p></div>
+            <?php
         }
-		return $input;
-	}
 
-	/** 
-	* Get the settings option array and print one of its values
-	*/
-	public function analyze_url_callback() {
-		printf( '<input type="text" id="analyze_url" name="se_option[analyze_url]" value="%s" size="70" />', esc_attr( $this->options['analyze_url'] ) );
-	}
+        //Get current options
+        $this->options = get_option('se_option');
 
-    public function title_generator_url_callback() {
-        printf( '<input type="text" id="title_generator_url" name="se_option[title_generator_url]" value="%s" size="70" />', esc_attr( $this->options['title_generator_url'] ) );
+        $html = <<<HTML
+   <div class="wrap">
+		<div id="icon-options-general" class="icon32"></div>
+		<h2>Social Ears Settings</h2>
+		<form method="post">
+            <table class="form-table">
+                <tbody>
+                    <tr valign="top">
+                        <th scope="row">Analyze URL</th>
+                        <td>
+                            <input type="text" id="analyze_url" name="se_option[analyze_url]" value="{$this->options['analyze_url']}" size="70">
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Title generator URL</th>
+                        <td>
+                            <input type="text" id="title_generator_url" name="se_option[title_generator_url]" value="{$this->options['title_generator_url']}" size="70">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+		    <p class="submit"><input type="submit" name="se_submit" id="submit" class="button button-primary" value="Save Changes"></p>
+		</form>
+    </div>
+HTML;
+        echo $html;
     }
+
 }
 
-if( is_admin() ){
-	$my_settings_page = new social_ears();
+if (is_admin()) {
+    $my_settings_page = new social_ears();
 }
